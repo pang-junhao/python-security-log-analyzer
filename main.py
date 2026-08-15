3422da7b0442f0cdbef956e8b00da644b7edb91c
pang-junhao <junhao2234@hamil.com>
2026-08-15 13:59:13 +0800

    Fix variable names

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from detector import detect_failed_logins
 
 ## Set log_file destination ##
 log_file = "logs/auth.log"
+threshold = 5
 
 # Call parser_log function and retrieve value as events #
 events = parser_log(log_file)
 
-# Call detect_failed_logins and retrieve value as detect #
-detect = detect_failed_logins(events, "5")
+# Call detect_failed_logins and retrieve value as suspicious_ips #
+suspicious_ips = detect_failed_logins(events, threshold)
